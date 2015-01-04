3162a8bddb290272ad535a3989f28255891f08bc
peiho <kkpietro@yahoo.com>
2015-01-04 18:08:57 +0200

    better structured JS
    code is moved into objects

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,3 @@
-	
-var rb = Rapidboard();
-var nav = [0];
-
-var wb;
 
 $(document).ready(function(){
 	init();
@@ -10,131 +5,33 @@ $(document).ready(function(){
 
 function init()
 {
-	//clear
+	//clear the body of the document
 	$("body").children().remove();
-	// main container
-	var main = document.createElement('div');
-	main.setAttribute('class','main');
 	
-	// headline with breadcrumbs
-	var bread = document.createElement('div');
-	bread.setAttribute('class','head');
-	wb = BreadCrumbWidget(bread);
-	main.appendChild(bread);	
+	// first build the html structure
 	
-	// category widget
-	var cat = document.createElement('div');
-	cat.setAttribute('class','cat');
-	main.appendChild(cat);
+	var mainElement = document.createElement('div'); //contains the whole page
+	mainElement.setAttribute('class','main');
+	var headElement = document.createElement('div'); //heading + breadcrumbs
+	headElement.setAttribute('class','head');
+	mainElement.appendChild(headElement);	
+	var catElement = document.createElement('div'); //category listing
+	catElement.setAttribute('class','cat');
+	mainElement.appendChild(catElement);
+	var contentElement = document.createElement('div'); //content
+	contentElement.setAttribute('class','content');
+	contentElement.textContent = "page content comes here";
+	mainElement.appendChild(contentElement);
 	
-	//content
-	var content = document.createElement('div');
-	content.setAttribute('class','content');
-	content.textContent = "page content comes here";
-	main.appendChild(content);
-	
-	catmain(cat);
-	wb.Refresh(bread);
+	//client side control
+	api = Rapidboard();
+	nav = Navigator();
+	wb = BreadCrumbWidget(headElement, nav, api);
+	wc = CategoryWidget(catElement, nav, api);
+	nav.AddListener(wb.Refresh);
+	nav.AddListener(wc.Refresh);
 	
 	// make it visible
-	document.body.appendChild(main);
-}
-
-function generateCategoryListItem(data)
-{
-	var e = document.createElement('li');
-	e.setAttribute('class','categoryListItem');
-	e.setAttribute('content',data.id);
-	e.onclick = function(){ nav.push(data.id); catamount=10; wb.Refresh(); catmain(); };
-	e.textContent = data.name;
-	return e;
-}
-
-function generateDefaultListItem(data)
-{
-	var e = document.createElement('li');
-	e.textContent = data;
-	return e;
-}
-
-function generateList(array, itemGenerator)
-{
-	var list = document.createElement('ul');
-	for (var i=0; i<array.length; i++)
-	{
-		list.appendChild(itemGenerator(array[i]));
-	}
-	return list;
-}
-
-function generateCategoryList(data)
-{
-	var list = generateList(data,generateCategoryListItem);
-	
-	if (nav.length>1)
-	{
-		var er = generateDefaultListItem('back');
-		er.setAttribute('class', 'categoryListItem categoryListFunction');
-		er.onclick = function(){ nav.pop(); wb.Refresh(); catmain(); };
-		list.appendChild(er);
-	}
-	
-	
-	var en = generateDefaultListItem('new');
-	en.setAttribute('class', 'categoryListItem categoryListFunction');
-	en.onclick = function(){
-		var form = document.createElement("form");
-		form.setAttribute('class', 'categoryCreateForm');
-		var inputname = document.createElement("input");
-		form.appendChild(inputname);
-		form.onsubmit = function(event){
-			var name = form.firstChild.value;
-			if (name.length>0)
-				rb.CategoryCreate(name, nav[nav.length-1], 
-					function(data){nav.push(data.id); catmain(); wb.Refresh();});
-			else 
-				catmain();
-			return false;
-		}
-		this.textContent = null;
-		this.appendChild(form);
-		this.onclick=null;
-		this.setAttribute('class', 'categoryListItemCreate'); 
-		
-	};
-	list.appendChild(en);
-	
-	var em = generateDefaultListItem('more');
-	em.setAttribute('class', 'categoryListItem categoryListFunction');
-	em.onclick = function(){
-		catamount *= 2;
-		catmain();
-	}
-	list.appendChild(em);
-	
-	return list;
-}
-
-var catelem;
-var catamount = 10;
-
-function catmain(element)
-{
-	if (element != null) catelem = element; else element = catelem;
-	var current = nav[nav.length-1];
-	//console.log(nav,current);
-	rb.CategoryList(current,catamount,function(list){
-		//console.log(list);
-		var html = generateCategoryList(list);
-		removeChildren(element);
-		element.appendChild(html);
-	})
-}
-
-
-function removeChildren(element)
-{
-	while (element.firstChild)
-		element.removeChild(element.firstChild);
+	document.body.appendChild(mainElement);
 }
 
--- public/javascripts/widgets.js
+++ public/javascripts/widgets.js
@@ -1,18 +1,208 @@
 
-var BreadCrumbWidget = function(target_element){
+function removeChildren(element)
+{
+	while (element.firstChild)
+		element.removeChild(element.firstChild);
+}
 
-	var element = target_element;
+/// 
+/// This object is used for hierarhical navigation
+/// When location changes, the list of functions is called from the lsitener
+/// list.
+///
+var Navigator = function(rootlocation, listenerList)
+{
+	if (rootlocation == null)
+		rootlocation = 0;
+	var rootloc = rootlocation;
+	var stack = [rootlocation];
+	var listeners;
+	if (listenerList != null)
+		listeners = listenerList;
+	else 
+		listeners = [];
+	
+	var locationChanged = function()
+	{
+		for (var i=0; i<listeners.length; i++)
+		{
+			listeners[i]();
+		}
+	}
+	
+	return {
+		Enter:function(node){
+			stack.push(node);
+			locationChanged();
+		},
+		Return:function(){
+			var oldlocation = stack.pop();
+			locationChanged();
+			return oldlocation;
+		},
+		ReturnTo:function(desiredLocation)
+		{
+			var i = stack.length - 1;
+			var loc = stack[i];
+			while (i>0 && stack[i] != desiredLocation) {
+				stack.pop();
+				i--;
+			}
+			if (loc != stack[stack.length - 1])
+				locationChanged();
+		},
+		Current:function(){
+			return stack[stack.length - 1];
+		},
+		IsRoot:function()
+		{
+			return Top() == rootloc;
+		},
+		AsList:function()
+		{
+			return stack;
+		},
+		AddListener:function(callback)
+		{
+			listeners.push(callback);
+		},
+		Count:function()
+		{
+			return stack.length;
+		}
+	}
+}
+
+
+///
+/// This object facilitates category navigation using html.
+/// It uses a navigator object and the rapidboard api.
+///
+var CategoryWidget = function(targetElement, navigatorInstance, apiInstance)
+{
+	var nav = navigatorInstance;
+	var api = apiInstance;
+	var element = targetElement;
+	var amount = 10;
+
+	var categoryListItemClicked = function()
+	{	
+		amount = 10;
+		nav.Enter(Number(this.getAttribute('content')));
+	}
+	
+	var generateCategoryListItem = function (data)
+	{
+		var e = document.createElement('li');
+		e.setAttribute('class','categoryListItem');
+		e.setAttribute('content',data.id);
+		e.textContent = data.name;
+		e.onclick = categoryListItemClicked;
+		return e;
+	}
+
+	var generateDefaultListItem = function(data)
+	{
+		var e = document.createElement('li');
+		e.textContent = data;
+		return e;
+	}
+
+	var generateList = function(array, itemGenerator)
+	{
+		var list = document.createElement('ul');
+		for (var i=0; i<array.length; i++)
+		{
+			list.appendChild(itemGenerator(array[i]));
+		}
+		return list;
+	}
+
+	var generateCategoryList = function(data)
+	{
+		var list = generateList(data,generateCategoryListItem);
+		
+		if (nav.Count() > 1)
+		{
+			var er = generateDefaultListItem('back');
+			er.setAttribute('class', 'categoryListItem categoryListFunction');
+			er.onclick = function(){ nav.Return(); };
+			list.appendChild(er);
+		}
+		
+		if (data.length<amount)
+		{
+			var en = generateDefaultListItem('new');
+			en.setAttribute('class', 'categoryListItem categoryListFunction');
+			en.onclick = function(){
+				var form = document.createElement("form");
+				form.setAttribute('class', 'categoryCreateForm');
+				var inputname = document.createElement("input");
+				form.appendChild(inputname);
+				form.onsubmit = function(event){
+					var name = form.firstChild.value;
+					if (name.length>0)
+						api.CategoryCreate(name, nav.Current(), 
+							function(data){nav.Enter(data.id)});
+					else 
+						refresh();
+					return false;
+				}
+				this.textContent = null;
+				this.appendChild(form);
+				this.onclick=null;
+				this.setAttribute('class', 'categoryListItemCreate'); 			
+		};
+		list.appendChild(en);
+		}
+		else
+		{
+			var em = generateDefaultListItem('more');
+			em.setAttribute('class', 'categoryListItem categoryListFunction');
+			em.onclick = function(){
+				amount *= 2;
+				refresh();
+			}
+			list.appendChild(em);
+		}
+		return list;
+	}
+	
+	var refresh = function(){
+		var current = nav.Current();
+		//console.log(nav,current);
+		api.CategoryList(current,amount,function(list){
+			//console.log(list);
+			var html = generateCategoryList(list);
+			removeChildren(element);
+			element.appendChild(html);
+		})
+	}
+	
+	refresh(); //initial render
+	
+	return {
+		Refresh : function(){
+			refresh();
+		}
+	}
+}
+
+///
+/// This object displays the navigator object using html
+/// and also allows breadcrumb-like navigation.
+///
+var BreadCrumbWidget = function(targetElement, navigatorInstance, apiInstance)
+{
+	var nav = navigatorInstance;
+	var api = apiInstance;
+
+	var element = targetElement;
 	
 	var breadItemClicked = function()
 	{
 		var val = Number(this.getAttribute('content'));
-		var i = nav.length - 1;
-		while (i>0 && nav[i] != val) {
-			nav.pop();
-			i--;
-		}
-		refresh();
-		catmain();
+		nav.ReturnTo(val);
 	}
 	
 	var generateBreadItem = function(name, target)
@@ -27,12 +217,13 @@ var BreadCrumbWidget = function(target_element){
 	
 	var refresh = function()
 	{
-		rb.CategoryNames(nav, function(data){
+		var list = nav.AsList();
+		api.CategoryNames(list, function(data){
 			var ul = document.createElement('div');
 			ul.setAttribute('class','bread');
 			
 			heading = document.createElement('h1');
-			heading.appendChild(generateBreadItem('rapidboard',nav[0]));
+			heading.appendChild(generateBreadItem('rapidboard',list[0]));
 			ul.appendChild(heading);
 			for (var i=1; i<data.length; i++)
 			{
@@ -40,12 +231,14 @@ var BreadCrumbWidget = function(target_element){
 				sep.textContent = ' / ';
 				sep.setAttribute('class', 'breadSep');
 				ul.appendChild(sep);
-				ul.appendChild(generateBreadItem(data[i],nav[i]));
+				ul.appendChild(generateBreadItem(data[i],list[i]));
 			}
 			removeChildren(element);
 			element.appendChild(ul);
 		});
 	}
+	
+	refresh(); //initial render
 
 	return {
 		Refresh : function ()
